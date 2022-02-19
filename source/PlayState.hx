0a4b7c2bacd3af70140dcb777b17916ae2ead913
thepercentageguy <54141759+thepercentageguy@users.noreply.github.com>
2022-02-19 19:04:43 +0100

    oopssss

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,19 +1,24 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
-	// map containing UFNF versions (DO NOT MODIFY FOR MODS)
-	var versions:Map<String, String> = [
-		"0.7.2 (Latest)" => "https://github.com/thepercentageguy/Unnamed-FNF-Engine/releases/download/v0.7.2/ufnf-0_7_2.zip"
-	];
-
-	// map containing UFNF mods (PUT YOUR MOD ON THIS)
-	var mods:Map<String, String> = [];
-
 	override public function create()
 	{
+		FlxG.camera.antialiasing = true;
+
+		var title = new FlxText(0, 10, FlxG.width, "UFNF Engine Launcher");
+		title.alignment = CENTER;
+		title.size = 32;
+		add(title);
+
+		var changelog = new FlxText(0, 70, FlxG.width - 10, "Changelogs:");
+		changelog.alignment = LEFT;
+		changelog.screenCenter(X);
+
 		super.create();
 	}
 
