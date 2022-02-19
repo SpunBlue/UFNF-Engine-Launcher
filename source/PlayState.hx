962f8caeb6665d885a284e4c609629f788ff02a3
thepercentageguy <54141759+thepercentageguy@users.noreply.github.com>
2022-02-19 17:55:32 +0100

    setup

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,12 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	// map containing UFNF versions (DO NOT MODIFY FOR MODS)
+	var versions:Map<String, String> = ["0.7.2 (Latest)" => ""];
+
+	// map containing UFNF mods (PUT YOUR MOD ON THIS)
+	var mods:Map<String, String> = [];
+
 	override public function create()
 	{
 		super.create();
