e6bb21dab1d5179dce19005abdc5636b4ba1aa1f
thepercentageguy <54141759+thepercentageguy@users.noreply.github.com>
2022-02-19 17:57:42 +0100

    oops

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,7 +5,9 @@ import flixel.FlxState;
 class PlayState extends FlxState
 {
 	// map containing UFNF versions (DO NOT MODIFY FOR MODS)
-	var versions:Map<String, String> = ["0.7.2 (Latest)" => ""];
+	var versions:Map<String, String> = [
+		"0.7.2 (Latest)" => "https://github.com/thepercentageguy/Unnamed-FNF-Engine/releases/download/v0.7.2/ufnf-0_7_2.zip"
+	];
 
 	// map containing UFNF mods (PUT YOUR MOD ON THIS)
 	var mods:Map<String, String> = [];
